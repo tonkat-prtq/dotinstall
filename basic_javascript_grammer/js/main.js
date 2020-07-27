1e447fc5364a6ad409adb1deb9fa27026465fcc8
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-07-27 21:30:30 +0900

    object 05

--- basic_javascript_grammer/js/main.js
+++ basic_javascript_grammer/js/main.js
@@ -2,9 +2,8 @@
 
 {
   const scores = [80, 90, 40, 70];
-  scores.push(60, 50);
-  scores.shift(); // 1つずつしか削除できない
-  // 90, 40, 70, 60, 50
+
+  scores.splice(1, 1, 40, 50)
 
   // for (let i = 0; i < 3; i++) {
   for (let i = 0; i < scores.length; i++) {
