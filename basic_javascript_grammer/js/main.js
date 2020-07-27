0a3b0030a5c4a5034ed27a116992d12430d095b7
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-07-27 21:23:30 +0900

    object 02

--- basic_javascript_grammer/js/main.js
+++ basic_javascript_grammer/js/main.js
@@ -2,5 +2,12 @@
 
 {
   const scores = [80, 90, 40];
-  console.log(scores);
+  // console.log(scores[1]);
+
+  // scores[2] = 44;
+  // console.log(scores);
+
+  // constの注意点:scoresそのものへの再代入はできないが、配列の要素そのものへの再代入は可能
+
+  console.log(scores.length);
 }
