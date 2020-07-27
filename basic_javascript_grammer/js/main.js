798dbbd60f20ddd7a87204020107359d47c4397a
Takahiro Kobayashi <k2t.y4kn@gmail.com>
2020-07-27 21:35:00 +0900

    object 07

--- basic_javascript_grammer/js/main.js
+++ basic_javascript_grammer/js/main.js
@@ -1,14 +1,26 @@
 'use strict';
 
 {
-  const otherScores = [10, 20];
-  const scores = [80, 90, 40, 70, ...otherScores];
-  // console.log(scores);
+  const scores = [80, 90, 40, 70];
 
-  function sum (a, b) {
-    console.log(a + b);
-  }
+  // const [a, b, c, d] = scores;
 
-  sum(...otherScores);
-  // sum(10, 20)と同じ意味
+  // console.log(a);
+  // console.log(b);
+  // console.log(c);
+  // console.log(d);
+
+  const[a, b, ...others] = scores;
+
+  console.log(a);
+  console.log(b);
+  console.log(others);
+
+  let x = 30;
+  let y = 70;
+  [x, y] = [y, x];
+
+  console.log(x);
+  console.log(y);
+
 }
